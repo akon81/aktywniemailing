3751c8e60660a61276e2dfac203486f1c6e6a5ce
Konrad <k.adamczyk81@gmail.com>
2026-03-26 12:59:50 +0100

    Zmie ikon scroll-to-top na strzak w gór (chevron up)

--- resources/views/components/scroll-to-top.blade.php
+++ resources/views/components/scroll-to-top.blade.php
@@ -5,7 +5,7 @@
     aria-label="{{ __('ui.scroll_to_top_label') }}"
     style="display: none;">
     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18M7 8V6a2 2 0 012-2h10a2 2 0 012 2v2"/>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
     </svg>
 </button>
 
